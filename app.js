b54b35e9966b872b16375e51ae0265a5f54ff7ea
Gary <garyarcediaz@gmail.com>
2019-06-08 22:44:05 -0400

    Servidor de suscripciones

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+var AWS = require('aws-sdk');
+const uuidv1 = require('uuid/v1');
+
+AWS.config.update({
+	accessKeyId: "AKIAJKWRMKS5VWVK2NFA", 
+	secretAccessKey: "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",
+	region: 'us-east-2'
+});
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/landing/subscriptions', function (req, res) {
+  	var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+    var params = {
+        TableName: 'inscripciones',
+        Item: {
+			'id': {S: uuidv1()},
+            'nombre' : {S: req.body.nombre},
+            'telefono' : {S: req.body.telefono},
+            'correo' : {S: req.body.correo},
+            'rut' : {S: req.body.rut}
+        }  
+	};  
+	
+    ddb.putItem(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+            res.json({
+				err: true,
+				msj: 'Problemas al insertar el documento.',
+				variant: 'danger'
+            });
+        } else {
+            console.log("Success", data);
+            res.json({
+				err: false,
+				msj: 'Se registró correctamente.',
+				variant: 'success'
+            });
+        }
+    });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
